9720bce29b75250fe7c8158c11579f4e44b5f3b8
Madumi Mandira <80471253+MandiraTD@users.noreply.github.com>
2024-11-01 20:07:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.use("/api/recipes", recipeRoutes);
 
 
 app.listen(PORT,  () => {
-    console.log(`Server is running on port ${HOST} ${PORT}`);
+    console.log(`Server is running on port  ${PORT}`);
 });
